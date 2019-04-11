b22568b30c86d88bb63480a2a740ea984384cea2
Karthik <eyank1195@gmail.com>
2019-04-11 19:45:24 +1000

    Add project files.

---
+++ VisionMelbourneV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisionMelbourneV3.Startup))]
+namespace VisionMelbourneV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
